555b457347e9d1bc395e73dcb719da989a69634e
myungbin <kki9621@naver.com>
2023-05-26 16:38:22 +0900

    Add Config file

---
+++ config/config.py
@@ -0,0 +1,7 @@
+class Config:
+    RESULT_PATH = r'C:\MB_Project\project\Competition\VISOL\mmdetection\configs\visol\result'
+    SAMPLE_SUBMISSION_PATH = r'C:\MB_Project\project\Competition\VISOL\data\sample_submission.csv'
+    TEST_IMAGE = r'C:\MB_Project\project\Competition\VISOL\data\test\*.png'
+    SUBMISSION_PATH = r'C:\MB_Project\project\Competition\VISOL\data\submission'
+
+cfg = Config()
